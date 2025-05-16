0b29f1ee539d6313950fb62c0853c50a46971b11
Teddy Setiady <teddysetiadi850@gmail.com>
2025-05-16 14:30:14 +0700

    V alpha 2

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,7 +1,7 @@
 import Home from './home.jsx'
 import Produksi from './Produksi.jsx'
 import Registrasi from './Registrasi.jsx'
-import Detail from './detail.jsx'
+import Detail from './Detail.jsx'
 import BahanBaku from './BahanBaku.jsx'
 
 export {Home, Produksi, Registrasi, Detail, BahanBaku}
